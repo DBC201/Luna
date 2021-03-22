4a1587f947ae8c3270694572b5f6ebcf057eb6ca
atillaturkmen <turkmenatilla522@gmail.com>
2021-03-22 19:36:26 +0300

    check new listing

--- scripts/check-new-listing/check-new-listing.py
+++ scripts/check-new-listing/check-new-listing.py
@@ -1,34 +1,59 @@
 import requests
+import os
+import time
 
-last_listing = input()
 
-tag = "1ej4hfo"
-target = 5
+def get_last_listing():
+    tag = "1ej4hfo"
+    target = 5
 
-r = requests.get('https://www.binance.com/en/support/announcement/c-48')
+    r = requests.get('https://www.binance.com/en/support/announcement/c-48')
 
-stack = []
-tags_seen = 0
+    stack = []
+    tags_seen = 0
 
-current_listing = ""
+    current_listing = ""
 
-for c in str(r.content):
-    if tags_seen == target:
-        if c == '"' or c == ">":
-            continue
-        if c == "<":
-            break
-        current_listing += c
-    for i in range(len(tag)):
-        if len(stack) == i and c == tag[i]:
-            stack.append(c)
-            if i == len(tag) - 1:
-                tags_seen += 1
+    for c in str(r.content):
+        if tags_seen == target:
+            if c == '"' or c == ">":
+                continue
+            if c == "<":
+                break
+            current_listing += c
+        for i in range(len(tag)):
+            if len(stack) == i and c == tag[i]:
+                stack.append(c)
+                if i == len(tag) - 1:
+                    tags_seen += 1
+                    stack.clear()
+                break
+            if i >= len(stack):
                 stack.clear()
-            break
-        if i >= len(stack):
-            stack.clear()
-            break
+                break
+    return current_listing
 
-if last_listing != current_listing:
-    print("New listing!!")
+
+def write_to_file(file_name, content):
+    f = open(file_name, "w")
+    f.write(content)
+    f.close()
+
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, "last-new-listing.txt")
+f = open(my_file, "r")
+last_listing = f.read()
+f.close()
+
+if last_listing == "":
+    last_listing = get_last_listing()
+    write_to_file(my_file, last_listing)
+
+while True:
+    current_listing = get_last_listing()
+    if current_listing != last_listing:
+        print(current_listing)
+        # Inform users here
+        write_to_file(my_file, current_listing)
+    time.sleep(60)
--- scripts/check-new-listing/listing-interval.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do last-new-listing > check-new-listing.py; sleep 3600; done
