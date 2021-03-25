2f2c39559173eb20b9c138744cf70d5b9ac233aa
atillaturkmen <turkmenatilla522@gmail.com>
2021-03-25 19:15:29 +0300

    scrape ticker name and time string

--- luna_scripts/listing_scripts/scrape_listings.py
+++ luna_scripts/listing_scripts/scrape_listings.py
@@ -6,37 +6,6 @@ import re
 import json
 
 
-def get_last_listing():
-    tag = "1ej4hfo"
-    target = 5
-
-    r = requests.get('https://www.binance.com/en/support/announcement/c-48')
-
-    stack = []
-    tags_seen = 0
-
-    current_listing = ""
-
-    for c in str(r.content):
-        if tags_seen == target:
-            if c == '"' or c == ">":
-                continue
-            if c == "<":
-                break
-            current_listing += c
-        for i in range(len(tag)):
-            if len(stack) == i and c == tag[i]:
-                stack.append(c)
-                if i == len(tag) - 1:
-                    tags_seen += 1
-                    stack.clear()
-                break
-            if i >= len(stack):
-                stack.clear()
-                break
-    return current_listing
-
-
 def write_to_file(file_name, content):
     f = open(file_name, "w")
     f.write(content)
@@ -44,32 +13,47 @@ def write_to_file(file_name, content):
 
 
 def read_last_listing(file_name):
-    with open(file_name, 'r') as file:
-        return file.read()
+    with open(file_name, 'r') as f:
+        return f.read()
 
 
 def scrape_titles():
     decoded_page = requests.get('https://www.binance.com/en/support/announcement/c-48').content.decode()
-    regex_str = "<script id=\"__APP_DATA\" type=\"application\/json\">.*?<\/script>"
-    raw_json = re.findall(regex_str, decoded_page)[0][len("<script id=\"__APP_DATA\" type=\"application/json\">"):len("</script>")*-1]
+    regex_str = r"<script id=\"__APP_DATA\" type=\"application\/json\">.*?<\/script>"
+    raw_json = re.findall(regex_str, decoded_page)[0][
+               len("<script id=\"__APP_DATA\" type=\"application/json\">"):len("</script>")*-1]
     j = json.loads(raw_json)
-    articles = [article['title'] for article in j["routeProps"]['b723']["navDataResource"][0]['articles']]
-    return articles
+    return j["routeProps"]['b723']["navDataResource"][0]['articles']
+
+
+def get_last_listing():
+    return scrape_titles()[0]["title"]
+
+
+def get_listing_time(code):
+    html = requests.get('https://www.binance.com/en/support/announcement/'+code).content.decode()
+    regex_str = r"Binance will list .* in the Innovation Zone and will open trading for .* trading pairs at (.*)(UTC)"
+    return re.search(regex_str, html).group(1)[:-2]
 
 
 if __name__ == '__main__':
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, "last-new-listing.txt")
-    write_to_file(my_file, scrape_titles()[0])
+    write_to_file(my_file, get_last_listing())
     with open("./mailing_list.txt", 'r') as file:
         emails = [email.strip() for email in file.readlines()]
 
     while True:
-        current_listing = scrape_titles()[0]
+        current_listing = get_last_listing()
         if current_listing != read_last_listing(my_file):
             message = "Subject: " + current_listing + '\n'
             message += "\nhttps://www.binance.com/en/support/announcement/c-48"
             for email in emails:
                 send_mail.send(email, message)
+            if "Innovation Zone" in current_listing:
+                ticker = re.search(r"\(.*?\)", current_listing).group(0)[1:-1]
+                t = get_listing_time(scrape_titles()[0]["code"])
+                # Then run the script
+                # Maybe email the user
             write_to_file(my_file, current_listing)
         time.sleep(60)
