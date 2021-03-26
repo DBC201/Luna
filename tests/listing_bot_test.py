62b79ed167585ece172046231c08b2f1aab3d613
DBC201 <denizbcakiroglu@gmail.com>
2021-03-26 18:21:36 -0400

    issues with market sell

--- tests/listing_bot_test.py
+++ tests/listing_bot_test.py
@@ -17,11 +17,11 @@ def return_parser():
     return parser
 
 
-args = return_parser().parse_args(sys.argv[1:])
-BUY_TYPE = args.buy_type.upper()
-SELL_TYPE = args.sell_type.upper()
-SPENDING_AMOUNT = args.spending_amount
-env_path = args.env_path
+#args = return_parser().parse_args(sys.argv[1:])
+BUY_TYPE = "ETH" # args.buy_type.upper()
+SELL_TYPE = "USDT" # args.sell_type.upper()
+SPENDING_AMOUNT = 100 # args.spending_amount
+env_path = "../.env.local" # args.env_path
 SYMBOL = BUY_TYPE + SELL_TYPE
 
 load_dotenv(dotenv_path=env_path)
@@ -50,39 +50,49 @@ def round_down(number, decimals):
 
 
 SPENDING_AMOUNT = round_down(SPENDING_AMOUNT, symbol_info["quoteAssetPrecision"])
-
+print(client.get_asset_balance(BUY_TYPE)["free"], client.get_asset_balance(SELL_TYPE)["free"])
+order = client.order_market_sell(symbol=SYMBOL, quantity=1)
+print(order)
+print(client.get_asset_balance(BUY_TYPE)["free"], client.get_asset_balance(SELL_TYPE)["free"])
+exit()
 
 def trade_callback(data):
     if data['p']:
         global IN, IN_PRICE, MAX_PRICE, DOUBLE_OUT, TRIPLE_OUT, DOUBLE_VAL, TRIPLE_VAL, BOTTOM_VAL
         global BUY_TYPE, SELL_TYPE, SPENDING_AMOUNT, BALANCE, BASE_PRECISION
         global START_TIME
+        print(client.get_open_orders(symbol=SYMBOL))
+        print(client.get_asset_balance(BUY_TYPE)["free"], client.get_asset_balance(SELL_TYPE)["free"], data['p'])
         current_price = float(data['p'])
         MAX_PRICE = max(current_price, MAX_PRICE)
         if not IN:
             IN_PRICE = float(data['p'])
             client.order_market_buy(symbol=SYMBOL, quoteOrderQty=SPENDING_AMOUNT)
-            DOUBLE_VAL = IN_PRICE * 2
-            TRIPLE_VAL = IN_PRICE * 3
-            BOTTOM_VAL = IN_PRICE * 0.95
+            DOUBLE_VAL = IN_PRICE * 1.00001
+            TRIPLE_VAL = IN_PRICE * 1.00002
+            BOTTOM_VAL = IN_PRICE * 0.99999
             BALANCE = round_down(float(client.get_asset_balance(asset=BUY_TYPE)["free"]), BASE_PRECISION)
             IN = True
             START_TIME = time.time()
         else:
             if current_price <= BOTTOM_VAL:
+                print("selling")
                 client.order_market_sell(symbol=SYMBOL, quantity=BALANCE)
                 sys.exit()
             elif not DOUBLE_OUT and current_price >= DOUBLE_VAL:
                 BALANCE = round_down(BALANCE*0.5, BASE_PRECISION)
                 client.order_market_sell(symbol=SYMBOL, quantity=BALANCE)
+                print("two")
                 DOUBLE_OUT = True
             elif not TRIPLE_OUT and current_price >= TRIPLE_VAL:
                 to_sell = round_down(BALANCE*0.75, BASE_PRECISION)
                 client.order_market_sell(symbol=SYMBOL, quantity=to_sell)
+                print("tri")
                 BALANCE -= to_sell
                 BALANCE = round_down(BALANCE, BASE_PRECISION)
                 TRIPLE_OUT = True
             elif DOUBLE_OUT and TRIPLE_OUT and current_price <= MAX_PRICE * 0.95: # i don't like this multiplication here slows down?
+                print("trail selling")
                 client.order_market_sell(symbol=SYMBOL, quantity=BALANCE)
                 sys.exit()
             elif time.time() - START_TIME >= 58:
