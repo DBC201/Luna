b9515695fa6d0873c7648f943feef226ceab1cc5
dbc201 <denizbcakiroglu@gmail.com>
2021-04-02 19:49:51 +0300

    reformat

--- tests/run_script_test.py
+++ tests/run_script_test.py
@@ -2,8 +2,7 @@ import shlex, subprocess
 
 if __name__ == '__main__':
     process = subprocess.Popen(
-        shlex.split(
-        "python ./datetime_test.py"), # python3 for linux
+        shlex.split("python ./datetime_test.py"), # python3 for linux
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
