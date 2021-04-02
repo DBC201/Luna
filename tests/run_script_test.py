c5d22e17834c2b92e6da193f5334bfb63f31c98f
dbc201 <denizbcakiroglu@gmail.com>
2021-04-02 19:49:15 +0300

    run using subprocess

--- tests/run_script_test.py
+++ tests/run_script_test.py
@@ -1,4 +1,13 @@
-import os
+import shlex, subprocess
 
 if __name__ == '__main__':
-    os.system("python ./datetime_test.py")
+    process = subprocess.Popen(
+        shlex.split(
+        "python ./datetime_test.py"), # python3 for linux
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    process.wait()
+    stdout, stderr = process.communicate()
+    print(stdout.decode())
