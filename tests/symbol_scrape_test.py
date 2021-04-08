2470e3342ee93a3e0eb07253800cfd477a828db2
dbc201 <denizbcakiroglu@gmail.com>
2021-04-09 00:06:22 +0300

    generalize regex

--- tests/symbol_scrape_test.py
+++ tests/symbol_scrape_test.py
@@ -3,6 +3,5 @@ from luna_scripts.listing_scripts.BinanceAnnouncementScrape import BinanceAnnoun
 if __name__ == '__main__':
     scrape = BinanceAnnouncementScrape()
     print(scrape.get_announcement())
-    print(scrape.get_coin_names())
     print(scrape.get_listing_date())
     print(scrape.get_symbols())
