44be642295d8a2b1dec43d9c37f8a3e0d0ca3140
dbc201 <denizbcakiroglu@gmail.com>
2021-04-13 11:27:36 +0300

    auto make folder

--- tests/log_listing_test.py
+++ tests/log_listing_test.py
@@ -9,8 +9,10 @@ client = Client(os.environ["api_key"], os.environ["api_secret"])
 
 
 if __name__ == '__main__':
-    symbol = "ethusdt"
+    symbol = "btcusdt"
     save_path = "../trades"
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
     process = subprocess.Popen(
         shlex.split(f"python ../luna_scripts/listing_scripts/log_listing.py {symbol} {save_path} {ENV_PATH} -d 3"),
         shell=True,
