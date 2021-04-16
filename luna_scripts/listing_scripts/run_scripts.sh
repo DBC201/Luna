b12c647eea6bd9d9814f27c257de0f4181b5c246
dbc201 <denizbcakiroglu@gmail.com>
2021-04-16 10:06:19 +0000

    bash script checks for trades folder

--- luna_scripts/listing_scripts/run_scripts.sh
+++ luna_scripts/listing_scripts/run_scripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+if [ ! -d "../../trades" ]; then
+	mkdir "../../trades"
+fi
 nohup python3 auto_mail.py > "../../outputs/mail_output.txt" &
 nohup python3 auto_log.py > "../../outputs/log_output.txt" &
 
