7f565d4171f73c280a5f2f4664b5287807426055
dbc201 <denizbcakiroglu@gmail.com>
2021-04-16 21:03:55 +0300

    fix new announcement

--- luna_scripts/listing_scripts/auto_mail.py
+++ luna_scripts/listing_scripts/auto_mail.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     while True:
         scraper.refresh()
         current_announcement = scraper.get_announcement()
-        announcement_is_new = current_announcement != "dsfasdf"
+        announcement_is_new = current_announcement != last_announcement
         if announcement_is_new:
             coins = scraper.get_symbols()
             if coins:
