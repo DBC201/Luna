38318debdbc85e33680febe1280b0a40a64706be
dbc201 <denizbcakiroglu@gmail.com>
2021-04-20 01:24:45 +0300

    graphing trade data

---
+++ luna_modules/trade_analysis/main.py
@@ -0,0 +1,18 @@
+import json
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    trades_path = "../../trades/"
+    file_name = "MIRUSDT_2021-04-19_11.00.00.json"
+    with open(trades_path+file_name, 'r') as fp:
+        json_data = json.load(fp)
+    x_axis = []
+    y_axis = []
+    start_time = json_data[0]['T']
+    for data in json_data:
+        x_axis.append((float(data['T'])-start_time)/1000)
+        y_axis.append(float(data['p']))
+    plt.plot(x_axis, y_axis)
+    plt.xlabel("seconds")
+    plt.ylabel("price")
+    plt.savefig(trades_path+file_name.replace(".json", ".png"))
