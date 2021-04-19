19ebf6c1827202b6749cf7c2ab04d0e2ed7419f4
dbc201 <denizbcakiroglu@gmail.com>
2021-04-19 17:02:18 +0300

    apply zombie fix to auto_log.py

--- luna_scripts/listing_scripts/auto_log.py
+++ luna_scripts/listing_scripts/auto_log.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     listing_time = None
     save_folder = "../../trades"
     symbols = None
-
+    active_processes = []
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
 
@@ -35,12 +35,16 @@ if __name__ == '__main__':
                 for quote in symbols[symbol]:
                     bot = subprocess.Popen(
                         shlex.split(f"python3 log_listing.py {symbol+quote} {save_folder} {ENV_PATH}"),
-                        # shell=True,
+                        shell=True,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE
                     )
+                    active_processes.append(bot)
             listing_time = None
             symbols = None
         last_announcement = current_announcement
         time.sleep(60)
+        for p in active_processes:
+            if p.poll() is None:
+                del p
