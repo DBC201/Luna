16f2f2fe46f8c897b117e5475f858604908b93d9
dbc201 <denizbcakiroglu@gmail.com>
2021-04-19 14:34:15 +0300

    test edit

--- tests/zombie_process_test.py
+++ tests/zombie_process_test.py
@@ -9,14 +9,16 @@ if __name__ == '__main__':
     ran = False
     start = time.time()
     while time.time() - start <= 15:
-        for listing in listings:
-            p = subprocess.Popen(
-                shlex.split(f"python3 ../luna_scripts/listing_scripts/auto_log.py {listing} {dump_path} {env_path} -d 3"),
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-            processes.append(p)
+        if not ran:
+            for listing in listings:
+                p = subprocess.Popen(
+                    shlex.split(f"python3 ../luna_scripts/listing_scripts/auto_log.py {listing} {dump_path} {env_path} -d 3"),
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
+                processes.append(p)
+            ran = True
         time.sleep(5)
         for p in processes:
             if p.poll() is None:
