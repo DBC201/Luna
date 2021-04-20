289c797a7cbb63c9f28bbf61e9da480b53470aba
dbc201 <denizbcakiroglu@gmail.com>
2021-04-20 12:34:26 +0300

    fix float

--- luna_modules/trade_analysis/main.py
+++ luna_modules/trade_analysis/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     y_axis = []
     start_time = json_data[0]['T']
     for data in json_data:
-        x_axis.append((float(data['T'])-start_time)/1000)
+        x_axis.append((data['T']-start_time)/1000)
         y_axis.append(float(data['p']))
     plt.plot(x_axis, y_axis)
     plt.xlabel("seconds")
