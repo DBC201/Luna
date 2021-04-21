b74c7ca825d3769843e1a824d26da1df81b6d459
dbc201 <denizbcakiroglu@gmail.com>
2021-04-21 13:54:05 +0300

    fix run script

--- luna_scripts/run_scripts.sh
+++ luna_scripts/run_scripts.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-current_path=$(pwd)
+current_path=$(realpath "$0")
 output_path="output_path/../../outputs"
 if [ ! -d output_path ]; then
 	mkdir $output_path
 fi
 
+cd "$current_path/listing_mail"
 nohup python3 auto_mail.py > "$output_path/mail_output.txt" &
+cd ..
+
+cd "$current_path/listing_log"
 nohup python3 auto_log.py > "$output_path/log_output.txt" &
+cd ..
