bd5fa413f6d669ea9de8c3551d192a6907a20a0a
atillaturkmen <turkmenatilla522@gmail.com>
2021-05-03 20:52:16 +0300

    first reST documentation

--- luna_scripts/meme/meme_main.py
+++ luna_scripts/meme/meme_main.py
@@ -24,6 +24,12 @@ emailWrapper = EmailWrapper(
 
 
 def send_bogdanoff(ticker):
+    """
+    Whales are out, inform everyone in mail list
+
+    :param ticker: ticker that will be sent in mail
+    :return:
+    """
     subject = "Dump EET --- " + ticker
     body = "https://www.binance.com/en/trade/" + ticker
     img = "dump/" + random.choice(os.listdir(os.path.join(ROOT, "luna_scripts", "meme", "dump")))
@@ -31,6 +37,11 @@ def send_bogdanoff(ticker):
 
 
 def send_jesse(ticker):
+    """
+    Whales are in, inform everyone in mail list
+
+    :param ticker: ticker that will be sent in mail
+    """
     subject = "Pump EET --- " + ticker
     body = "https://www.binance.com/en/trade/" + ticker
     img = "pump/" + random.choice(os.listdir((os.path.join(ROOT, "luna_scripts", "meme", "pump"))))
@@ -38,6 +49,11 @@ def send_jesse(ticker):
 
 
 def get_vitalik_on_the_line(ticker):
+    """
+    Put Vitalik on zhe line
+
+    :param ticker: ticker that will be sent in mail
+    """
     subject = "Get Vitalik On The Line --- " + ticker
     body = "https://www.binance.com/en/trade/" + ticker
     img = "vitalik/" + random.choice(os.listdir((os.path.join(ROOT, "luna_scripts", "meme", "vitalik"))))
