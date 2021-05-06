6562c53be523ec6b582c8491733b8e806cf1a1a7
dbc201 <denizbcakiroglu@gmail.com>
2021-05-06 20:07:22 +0300

    announcementscrape documentation

--- luna_modules/binance/BinanceAnnouncementScrape.py
+++ luna_modules/binance/BinanceAnnouncementScrape.py
@@ -4,6 +4,14 @@ import json
 
 
 class BinanceAnnouncementScrape:
+    """Scrape new announcements
+
+    Scrapes new announcements from binance new crypto listings page
+    (https://www.binance.com/en/support/announcement/c-48)
+
+    :param link: link to scrape from, reccomended to be left empty
+    :link type: string
+    """
     def __init__(self, link="https://www.binance.com/en/support/announcement/c-48"):
         self.link = link
         self.__last_title = self.__scrape_titles()[0]
@@ -11,6 +19,11 @@ class BinanceAnnouncementScrape:
         self.__symbols_date = self.__get_symbols_date()
 
     def __scrape_titles(self):
+        """Scrape the latest headings from page
+
+        :return titles:
+        :rtype: list
+        """
         decoded_page = requests.get(self.link).content.decode()
         regex_str = r"<script id=\"__APP_DATA\" type=\"application\/json\">.*?<\/script>"
         raw_json = re.findall(regex_str, decoded_page)[0][
@@ -19,6 +32,11 @@ class BinanceAnnouncementScrape:
         return j["routeProps"]['b723']["navDataResource"][0]['articles']
 
     def __get_symbols_date(self):
+        """Get the sentence that has the trading pairs and the date
+
+        :return pairs and date sentence:
+        :rtype: string
+        """
         html = requests.get(self.__last_link).content.decode()
         regex_str = r"open trading for (\w+\/\w+,?\s?)+(and\s\w+\/\w+\s)?trading pairs at \d+-\d+-\d+ \d+:\d+:?\d* \w\w \(.*?\)"
         search_result = re.search(regex_str, html)
@@ -28,6 +46,13 @@ class BinanceAnnouncementScrape:
             return None
 
     def get_symbols(self):
+        """Get the symbols to be listed, return none if no symbols
+
+        Scrapes the sentence that has the date and the pairs
+
+        :return: symbols
+        :rtype: dict
+        """
         if self.__symbols_date is None:
             return None
         regex_str = r"(\w+\/\w+,?\s?)+(and\s\w+\/\w+\s)?"
@@ -46,6 +71,13 @@ class BinanceAnnouncementScrape:
         return symbols
 
     def get_listing_date(self):
+        """Get the listing date
+
+        Scrapes the sentence that has date and pairs
+
+        :return: date
+        :rtype: string
+        """
         if self.__symbols_date is None:
             return None
         regex_str = r"\d+-\d+-\d+ \d+:\d+:?\d* \w\w \(.*?\)"
@@ -56,12 +88,28 @@ class BinanceAnnouncementScrape:
             return None
 
     def refresh(self, prev_index=0):
+        """ Refresh the page to check for new listing
+
+        :param prev_index: set the title to be scraped to nth (0 by default for latest)
+        :type prev_index: int
+        :return:
+        """
         self.__last_title = self.__scrape_titles()[prev_index]
         self.__last_link = 'https://www.binance.com/en/support/announcement/' + self.__last_title["code"]
         self.__symbols_date = self.__get_symbols_date()
 
     def get_announcement(self):
+        """Get the latest title
+
+        :return: latest title
+        :rtype: string
+        """
         return self.__last_title["title"]
 
     def get_announcement_link(self):
+        """Get the link to latest title
+
+        :return: url link to latest title
+        :rtype: string
+        """
         return self.__last_link
