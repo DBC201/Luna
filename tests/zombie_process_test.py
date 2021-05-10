6cfe9f301e73b5bb0110b0299bcdf865c3319b34
dbc201 <denizbcakiroglu@gmail.com>
2021-05-11 02:30:21 +0300

    fix command

--- tests/zombie_process_test.py
+++ tests/zombie_process_test.py
@@ -5,7 +5,6 @@ import os
 if __name__ == '__main__':
     processes = []
     listings = ["btcusdt", "ethusdt", "nanousdt"]
-    env_path = "../.env.local"
     dump_path = '../trades'
     if not os.path.isdir(dump_path):
         os.mkdir(dump_path)
@@ -14,7 +13,7 @@ if __name__ == '__main__':
         if not ran:
             for listing in listings:
                 p = subprocess.Popen(
-                    shlex.split(f"python3 ../luna_scripts/listing_log/log_listing.py {listing} {dump_path} {env_path} -d 3"),
+                    shlex.split(f"python3 ../luna_scripts/listing_log/log_listing.py {listing} {dump_path} -d 3"),
                     stdin=subprocess.PIPE,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
