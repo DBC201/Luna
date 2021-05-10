40bcbbb0726948ce876d45b54fd0c9722e93be71
dbc201 <denizbcakiroglu@gmail.com>
2021-05-11 02:56:46 +0300

    subprocess standalone

--- luna_scripts/listing_log/auto_log.py
+++ luna_scripts/listing_log/auto_log.py
@@ -13,8 +13,12 @@ if __name__ == '__main__':
     last_announcement = scraper.get_announcement()
     listing_time = None
     save_folder = os.path.join(ROOT, "trades")
+    process_output_folder = os.path.join(ROOT, "outputs")
     symbols = {}
-    active_processes = []
+
+    if not os.path.exists(process_output_folder):
+        os.mkdir(process_output_folder)
+
     if not os.path.exists(save_folder):
         os.mkdir(save_folder)
 
@@ -31,35 +35,27 @@ if __name__ == '__main__':
         if listing_time is not None and time.time() + 60 >= listing_time:
             for symbol in symbols:
                 for quote in symbols[symbol]:
-                    bot = subprocess.Popen(
-                        shlex.split(f"python3 log_listing.py {symbol + quote} {save_folder}"),
+                    process_output = os.path.join(process_output_folder, symbol+quote+"_binance_log.txt")
+                    subprocess.Popen(
+                        shlex.split(f"python3 log_listing.py {symbol + quote} {save_folder} > "
+                                    f"{process_output}"),
                         shell=True,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         text=True
                     )
-                    gate = subprocess.Popen(
-                        shlex.split(f"python3 gateio_log.py {symbol + '_' + quote} {save_folder}"),
+                    process_output = os.path.join(process_output_folder, symbol+quote+"_gateio_log.txt")
+                    subprocess.Popen(
+                        shlex.split(f"python3 gateio_log.py {symbol + '_' + quote} {save_folder} > "
+                                    f"{process_output}"),
                         shell=True,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         text=True
                     )
-                    active_processes.append([symbol+quote, bot])
-                    active_processes.append([symbol+'_'+quote, gate])
             listing_time = None
             symbols.clear()
         last_announcement = current_announcement
         time.sleep(60)
-        for i in active_processes:
-            name, p = i
-            if p.poll() is None:
-                stdout, stderr = p.communicate()
-                if stdout:
-                    print(name, "stdout:", stdout)
-
-                if stderr:
-                    print(name, "stderr:", stderr)
-                del i
--- tests/zombie_process_test.py
+++ tests/zombie_process_test.py
@@ -12,25 +12,15 @@ if __name__ == '__main__':
     while True:
         if not ran:
             for listing in listings:
-                p = subprocess.Popen(
-                    shlex.split(f"python3 ../luna_scripts/listing_log/binance_log.py {listing} {dump_path} -d 3"),
+                subprocess.Popen(
+                    shlex.split(
+                        f"python3 ../luna_scripts/listing_log/binance_log.py {listing} "
+                        f"{dump_path} -d 3 > ../outputs/{listing}.txt"
+                    ),
                     stdin=subprocess.PIPE,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                     text=True
                 )
-                processes.append([listing, p])
             ran = True
         time.sleep(5)
-        print(processes)
-        for i in processes:
-            name, p = i
-            stdout, stderr = p.communicate()
-            if stdout:
-                print(name, "stdout:", stdout)
-
-            if stderr:
-                print(name, "stderr:", stderr)
-            p.kill()
-        p = None
-        processes.clear()
