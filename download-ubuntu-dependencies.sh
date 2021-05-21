6d3c6e4af1b13382f93662619f7529398c9e71b3
dbc201 <denizbcakiroglu@gmail.com>
2021-05-21 20:53:42 +0300

    remove shell script

--- download-ubuntu-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt install python3-pip  # check if pip is installed
-
-# install the listed dependencies
-pip3 install python-dotenv
-pip3 install python-binance
-pip3 install websocket-client
-pip3 install matplotlib
