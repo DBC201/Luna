c32fb275d2d671436b3699f164d12f38995cd574
dbc201 <denizbcakiroglu@gmail.com>
2021-09-15 23:45:07 -0400

    seperate folders for binance and gate io both under historical trades

--- luna_scripts/listing_log/download_binance.py
+++ luna_scripts/listing_log/download_binance.py
@@ -9,7 +9,7 @@ ROOT = os.path.join(os.path.dirname(__file__), "..", "..")
 
 load_dotenv(dotenv_path=os.path.join(ROOT, ".env.local"))
 client = Client(api_key=os.environ["api_key"], api_secret=os.environ["api_secret"])
-DUMP_FOLDER = os.path.join(ROOT, "historical_trades")
+DUMP_FOLDER = os.path.join(ROOT, "historical_trades/binance")
 
 
 def dump_minute_trades(symbol):
