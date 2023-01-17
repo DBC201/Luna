5c6e03d2e9723d7a3795d857384138bb48dbd191
dbc201 <denizbcakiroglu@gmail.com>
2023-01-17 13:12:21 -0500

    fix scraper

--- luna_scripts/kill_scripts.sh
+++ luna_scripts/kill_scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# mail_pid=$(ps aux | grep python3 | awk '{if ($12 == "auto_mail.py"){print $2}}')
-# sudo kill -9 $mail_pid
+mail_pid=$(ps aux | grep python3 | awk '{if ($12 == "auto_mail.py"){print $2}}')
+sudo kill -9 $mail_pid
 # log_pid=$(ps aux | grep python3 | awk '{if ($12 == "auto_log.py"){print $2}}')
 # sudo kill -9 $log_pid
-meme_pid=$(ps aux | grep python3 | awk '{if ($12 == "DiscordBot.py"){print $2}}')
-sudo kill -9 $meme_pid
+# meme_pid=$(ps aux | grep python3 | awk '{if ($12 == "DiscordBot.py"){print $2}}')
+# sudo kill -9 $meme_pid
 
 echo "Active python3 processes:"
 ps aux | grep python3
--- luna_scripts/run_scripts.sh
+++ luna_scripts/run_scripts.sh
@@ -5,18 +5,18 @@ if [ ! -d "$output_path" ]; then
 	mkdir "$output_path"
 fi
 
-# cd "$current_path/listing_mail"
-# nohup python3 auto_mail.py > "$output_path/mail_output.txt" &
-# cd ..
+cd "$current_path/listing_mail"
+nohup python3 auto_mail.py > "$output_path/mail_output.txt" &
+cd ..
 
 cd "$current_path/listing_log"
 python download_gateio.py > ../../outputs/download_gateio.txt &
 # nohup python3 auto_log.py > "$output_path/log_output.txt" &
 cd ..
 
-cd "$current_path/meme"
-nohup python3 DiscordBot.py > "$output_path/meme_output.txt" &
-cd ..
+# cd "$current_path/meme"
+# nohup python3 DiscordBot.py > "$output_path/meme_output.txt" &
+# cd ..
 
 # echo "Active python3 processes:"
 # ps aux | grep python3
